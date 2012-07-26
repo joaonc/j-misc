8c98a169fe5b648ebb3dae39541e2d88f04172c2
joaonc@gmail.com <joaonc@gmail.com@71b7c5b8-200b-d77d-86d0-2876b8b0ad81>
2012-07-26 20:58:00 +0000

    Adding the "2x1 keep ratio" Photoshop javascript that places two images side-by-side (two columns, one row) with the same height and keeping the aspect ratio. Width and distance between images is configurable. Height is calculated. This script needs to be run inside Photoshop and works in Windows or Mac.
    Tested on Windows with Photoshop CS4, but should work with other versions.

---
+++ trunk/Photoshop/2x1 keep ratio.jsx
@@ -0,0 +1,95 @@
+// This script aligns two images side by side to a fixed width and variable height
+// keeping the aspect ratio of both
+//
+// By Joao Coelho, July 7, 2012
+//
+// Created and tested with Photoshop CS4
+// probably runs with other versions as well, but not tested
+//
+
+var W = 750 // final width in pixels
+var D = 6   // distance between images in pixels
+
+var debug = false
+
+// Files to open
+// NOTE: The ExtendScript File object expects Universal Resource Identifier (URI) notation. See the JavaScript Tools Guide for more information.
+var fileNames = ["/C/Users/Public/Shared/Documents/Posts/1.jpg", "/C/Users/Public/Shared/Documents/Posts/2.jpg"];
+var newFileName = prompt("Filename (cancel for default)", "")
+var createNewFileName = (newFileName == null || newFileName.length == 0)
+if (createNewFileName) {
+	newFileName = ""
+}
+
+// Remember current unit settings and then set units to the value expected by this script
+var originalRulerUnits = preferences.rulerUnits
+var originalTypeUnits = preferences.typeUnits
+var originalDisplayDialogs = displayDialogs
+preferences.rulerUnits = Units.PIXELS
+preferences.typeUnits = TypeUnits.PIXELS
+displayDialogs = DialogModes.NO
+
+var w = []
+var h = []
+var fileDocs = []
+// Open files and get dimensions
+for (var i=0; i<fileNames.length; i++) {
+	if (debug) { alert("File " + (i+1) + ": " + fileNames[i]) }
+	var file = File(fileNames[i])
+	open(file)
+	fileDocs.push(activeDocument)
+	w.push(activeDocument.width)
+	h.push(activeDocument.height)
+	
+	if (createNewFileName) {
+		newFileName = newFileName + (newFileName.length == 0 ? "" : "-") + fileDocs[i].name
+	}
+}
+
+// Calculate final dimensions
+var wFinal = []
+wFinal.push(h[1]*w[0]*(W-D)/(h[0]*w[1]+h[1]*w[0]))
+wFinal.push(W-D-wFinal[0])
+
+var hFinal = wFinal[0]*h[0]/w[0]
+
+// Create new document
+var newDoc = documents.add(W, hFinal, 72, newFileName, NewDocumentMode.RGB, DocumentFill.TRANSPARENT, 1)
+for (var i=0; i<fileDocs.length; i++) {
+	if (debug) { alert("Working with file " + (i+1) + ": " + fileDocs[i].name) }
+	
+	// Flatten the document so we get everything and then copy
+	activeDocument = fileDocs[i]
+	fileDocs[i].flatten()
+	fileDocs[i].selection.selectAll()
+	fileDocs[i].selection.copy()
+
+	// Paste to the new doc (in a new layer)
+	activeDocument = newDoc
+	newDoc.paste()
+	newDoc.activeLayer.name = (i+1).toString() + "-" + fileDocs[i].name
+	
+	// Don’t save anything we did
+	fileDocs[i].close(SaveOptions.DONOTSAVECHANGES)
+}
+
+// Resize and move layers
+var x = 0
+activeDocument = newDoc
+for (var i=0; i<newDoc.artLayers.length; i++) {
+	var layer = newDoc.artLayers[i]
+	newDoc.activeLayer = layer
+	var bounds = layer.bounds
+	
+	if (debug) { alert(bounds.toString()) }
+	
+	layer.resize(wFinal[i]*100/(bounds[2]-bounds[0]), hFinal*100/(bounds[3]-bounds[1]), AnchorPosition.TOPLEFT)
+	bounds = layer.bounds
+	layer.translate(x-bounds[0], -bounds[1])
+	x = x + bounds[2] + D
+}
+
+// Restore original unit settings
+preferences.rulerUnits = originalRulerUnits
+preferences.typeUnits = originalTypeUnits
+displayDialogs = originalDisplayDialogs
